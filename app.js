04bafab488d5c0a1b2bc2987fc61be4021dcd01f
bismanxdhillxn <123344647+bismanxdhillxn@users.noreply.github.com>
2024-10-22 15:01:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 // app.js
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
-
+let tasks = [];
 app.set('view engine', 'ejs');
-
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.get('/welcome', (req, res) => {
@@ -12,7 +13,18 @@ app.get('/welcome', (req, res) => {
 
     res.render('welcome', { user });
 });
+app.get('/todo', (req, res) => {
+    res.render('todo', { tasks: tasks });
+});
 
+
+app.post('/add-task', (req, res) => {
+    const task = req.body.task; 
+    if (task) {
+        tasks.push(task); 
+    }
+    res.redirect('/todo');
+});
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
 });
